ee7e5cf6c7412677368fe79bbdc7e5cc3fa4fb41
jayce-kim1123 <kimjisung777@gmail.com>
2022-02-13 20:50:17 +0900

    MIGRATE: make migrations

--- products/models.py
+++ products/models.py
@@ -6,13 +6,13 @@ class Menu(models.Model):
     class Meta:
         db_table = 'menus'
 
-class Category(models.model):
+class Category(models.Model):
     name = models.CharField(max_length=45)
     menu = models.ForeignKey('Menu', on_delete=models.CASCADE)    
     class Meta:
         db_table = 'categories'
 
-class Drink(models.model):
+class Drink(models.Model):
     korean_name  = models.CharField(max_length=45)
     english_name = models.CharField(max_length=45)
     description  = models.TextField()
@@ -20,38 +20,38 @@ class Drink(models.model):
     class Meta:
         db_table = 'drinks'
 
-class Allergy(models.model):
+class Allergy(models.Model):
     name = models.CharField(max_length=45)
     class Meta:
-        db_table = 'allergy'
+        db_table = 'allergies'
 
-class Allergy_drink(models.model):
+class Allergy_drink(models.Model):
     allergy = models.ForeignKey('Allergy', on_delete=models.CASCADE)    
     drink   = models.ForeignKey('Drink'  , on_delete=models.CASCADE)    
     class Meta:
-        db_table = 'allergy_drink'
+        db_table = 'allergy_drinks'
 
-class Size(models.model):
+class Size(models.Model):
     name             = models.CharField(max_length=45)
     size_ml          = models.CharField(max_length=45, null=True)
     size_fluid_ounce = models.CharField(max_length=45, null=True)
     class Meta:
         db_table = 'sizes'
 
-class Nutrition(models.model):
+class Nutrition(models.Model):
     one_serving_kca = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     sodium_mg       = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     saturated_fat_g = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     sugars_g        = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     protein_g       = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     caffine_mg      = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    drink_mg        = models.ForeignKey('Drink', on_delete=CASCADE, null=True)
-    size_id         = models.ForeignKey('Size' , on_delete=CASCADE, null=True)
+    drink           = models.OneToOneField('Drink', on_delete=models.CASCADE, null=True)
+    size            = models.ForeignKey('Size' , on_delete=models.CASCADE, null=True)
     class Meta:
         db_table = 'nutritions'
 
-class Image(models.model):
-    Image_url = models.CharField(max_length=2000)
-    drink     = models.ForeignKey('Drink', on_delete=CASCADE)
+class Image(models.Model):
+    image_url = models.CharField(max_length=2000)
+    drink     = models.ForeignKey('Drink', on_delete=models.CASCADE)
     class Meta:
         db_table = 'images'
